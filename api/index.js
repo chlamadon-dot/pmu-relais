d6013eea5c97fc5c45884e06c0f734dccc40b23b
chlamadon-dot <chlamadon@gmail.com>
2026-02-08 17:22:01 +0100

    Refactor handler function and update response message

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-export default async function handler(req, res) {
-    // Autorisations de sécurité totales pour éviter les blocages PC
+export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-    // On envoie uniquement les chiffres, rien d'autre
-    res.status(200).send("12 - 5 - 1 - 18 - 4");
+    res.status(200).send("OUI-FONCTIONNE");
 }
