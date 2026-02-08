1294b09442c4ae1bbd4eb76fc2ae9d5b6a1f0198
chlamadon-dot <chlamadon@gmail.com>
2026-02-08 17:22:48 +0100

    Make handler function asynchronous and add CORS support
    Updated handler function to be asynchronous and added CORS headers.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+    // Force l'acceptation de la connexion depuis ton PC
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send("OUI-FONCTIONNE");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    
+    // Message de test ultra-simple
+    res.status(200).send("CONNEXION_OK");
 }
